e92ad5d46f9935fb527f2bf2fda66193b2be9e99
aehamdy <ae.hamdy91@gmail.com>
2024-11-16 12:01:44 +0200

    Style: Adjust RightHeaderPanel on different screens

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,7 +1,7 @@
 function MenuButton() {
     return(
-        <button type="button" className="group flex items-center gap-3">
-            <div className="uppercase">Menu</div>
+        <button type="button" className="group flex flex-wrap items-center gap-3">
+            <div className="hidden md:block uppercase">Menu</div>
             <div className="flex flex-col gap-1.5 w-7">
                 <span className="w-full h-1 bg-white"></span>
                 <span className="w-1/2 group-hover:w-full h-1 bg-white duration-short"></span>
--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -2,7 +2,7 @@ import searchIcon from "../assets/search-icon.svg";
 
 function SearchButton() {
     return (
-        <button type="button" className="hidden lg:block">
+        <button type="button" className="hidden md:block">
             <img src={searchIcon} alt="Search" className="" />
         </button>
     )
