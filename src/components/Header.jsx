a882e4ff0fac4df939e0821d0f4df4bc77fea27a
aehamdy <ae.hamdy91@gmail.com>
2024-11-16 10:46:13 +0200

    Style: Adjust body, root element and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import RightHeaderPanel from "./RightHeaderPanel";
 
 function Header() {
     return(
-        <header className="flex justify-between items-center ">
+        <header className="flex justify-between items-center py-8">
             <LeftHeaderPanel />
             <Logo />
             <RightHeaderPanel />
