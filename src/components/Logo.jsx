52112d90a1834ddbe34fb64284e792c2e1ee22b0
aehamdy <ae.hamdy91@gmail.com>
2024-11-16 12:08:48 +0200

    Style: Adjust SpinningLogo and Logo on different screens

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 function Logo() {
     return (
-      <a href="#" aria-label="Company Logo" className="relative text-4xl md:text-6xl lg:text-7xl text-white before:absolute before:w-[4px] before:h-[5px] md:before:w-[6px] md:before:h-[7px] lg:before:w-[8px] lg:before:h-[8px] before:bg-accent before:top-[75%] md:before:top-[78%] before:start-[13%] md:before:start-[13%] lg:before:start-[13.3%] select-none">
+      <a href="#" aria-label="Company Logo" className="relative text-4xl md:text-6xl lg:text-7xl text-white before:absolute before:w-[4px] before:h-[5px] md:before:w-[6px] md:before:h-[7px] lg:before:w-[8px] lg:before:h-[8px] before:bg-accent before:top-[75%] md:before:top-[78%] lg:before:top-[75%] before:start-[13%] md:before:start-[13%] lg:before:start-[13.1%] select-none">
         gleamy
       </a>
     );
--- src/components/SpinningLogo.jsx
+++ src/components/SpinningLogo.jsx
@@ -3,7 +3,7 @@ import circularLogo from "../assets/circular-logo.png";
 function SpinningLogo() {
     return (
         <div className="absolute flex items-center justify-center">
-            <img src={circularLogo} alt="Circular Logo" className="w-28 h-28 animate-rotate-slow" />
+            <img src={circularLogo} alt="Circular Logo" className="w-20 md:w-24 lg:w-28 h-20 md:h-24 lg:h-28 animate-rotate-slow" />
         </div>
     );
 }
