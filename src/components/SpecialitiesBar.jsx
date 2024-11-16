bb04ac867b61d39931ab4376d6be8fdc3b54a3fb
aehamdy <ae.hamdy91@gmail.com>
2024-11-16 22:48:39 +0200

    Style: Hide SpecialitiesBar on small screen and display it from medium screen and above

--- src/components/SpecialitiesBar.jsx
+++ src/components/SpecialitiesBar.jsx
@@ -1,6 +1,6 @@
 function SpecialitiesBar() {
     return(
-        <div className="vertical-text flex gap-6 w-fit mt-4 font-roboto text-xs text-white uppercase rotate-180">
+        <div className="vertical-text hidden md:flex gap-6 w-fit mt-4 font-roboto text-xs text-white uppercase rotate-180">
             <span>development</span>
             <span>designing</span>
             <span>digital marketing</span>
