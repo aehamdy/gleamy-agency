c48d5fb5162ac83f932d565a67021b78ae769243
aehamdy <ae.hamdy91@gmail.com>
2024-11-17 16:03:53 +0200

    Chore: Update ServiceCard to recieve heading text as a prop

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 import ServiceCardButton from "./ServiceCardButton";
 
-function ServiceCard() {
+function ServiceCard({ headingText }) {
     return(
         <article className="flex flex-col gap-3 text-center md:text-start">
-            <h3 className="text-white text-2xl">Brand Identity</h3>
+            <h3 className="text-white text-2xl">{headingText}</h3>
             <p className="text-paragraph-color leading-7">Bringing the history of your brand to the forefront gives an emotional dimension to your visual identity, which is essential today more than ever in today's digital landscape.</p>
             <ServiceCardButton />
         </article>
