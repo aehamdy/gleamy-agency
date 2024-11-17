1ff7185f6267a1bd80346865206574dac5a228b1
aehamdy <ae.hamdy91@gmail.com>
2024-11-17 17:41:23 +0200

    Chore: Cancel horizontal scroll on large screen

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,7 +4,7 @@ function Services() {
     const servicesHeadings = ["Brand Identity", "Technology", "UI/UX Design"];
 
     return (
-        <section className="flex gap-8 max-w-full py-4 overflow-x-scroll lg:overflow-x-auto snap-x">
+        <section className="flex gap-8 max-w-full py-4 overflow-x-scroll lg:overflow-x-hidden snap-x lg:snap-none">
             {servicesHeadings.map((heading, i) => (
                 <ServiceCard key={i} headingText={heading} />
             ))}
