e45fc838ae3fe4df0e70999101a1db9ad8d68af0
aehamdy <ae.hamdy91@gmail.com>
2024-11-19 14:41:59 +0200

    Style: Add z-inex for SpinningLogo and WatchVideoButton

--- src/components/WatchVideoButton.jsx
+++ src/components/WatchVideoButton.jsx
@@ -2,7 +2,7 @@ import buttonImage from "../assets/watch-video.svg";
 
 function WatchVideoButton() {
     return(
-        <button type="button" className="absolute bottom-1 md:bottom-2 end-0 md:end-4 lg:end-8 w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24">
+        <button type="button" className="absolute bottom-1 md:bottom-2 end-0 md:end-4 lg:end-8 w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 z-10">
             <img src={buttonImage} alt="Play Image" />
         </button>
     )
