18d661199e91ef6b33aec7c5cc72e7b747e8567d
aehamdy <ae.hamdy91@gmail.com>
2024-11-19 14:15:11 +0200

    Style: Adjust the position of MarqueeBar on different screens, and Adjust HeroHeading width on different screens

--- src/components/HeroHeading.jsx
+++ src/components/HeroHeading.jsx
@@ -1,7 +1,7 @@
 function HeroHeading() {
     return(
-        <h2 className="break-words w-full max-w-[19ch] text-white text-[2.5rem] sm:text-[3rem] md:text-[3.5rem] lg:text-[4rem] leading-tight z-10">
-            Brand.<span className="text-[#7e7e7e]">Design</span>.<span className="text-[#7e7e7e]">Product</span>. In-Hous Development. &More
+        <h2 className="break-words w-[90%] lg:w-[70%] max-w-[19ch] text-white text-[2.5rem] sm:text-[3rem] md:text-[3.5rem] lg:text-[4rem] leading-tight z-10">
+            Brand . <span className="text-[#7e7e7e]">Design</span> . <span className="text-[#7e7e7e]">Product</span> . In-Hous Development. &More
         </h2>
     )
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,10 +6,14 @@ function LandingPage() {
   return (
     <section className="relative flex flex-col gap-8 text-start mt-[25%] md:mt-[5%]">
       <HeroHeading />
-      <MarqueeBar variant="text-white bg-[#212121] rotate-[45deg] bottom-0 end-0 -translate-x-[14rem]" />
-      <MarqueeBar variant="text-black bg-white -rotate-[20deg] -bottom-[8rem] end-0 " />
+      <MarqueeBar variant="text-black bg-white rotate-[45deg] md:rotate-[45deg] bottom-0 end-0 -translate-x-[9rem] md:-translate-x-[25rem]" />
+      <MarqueeBar variant="text-white bg-[#212121] -rotate-[30deg] md:-rotate-[25deg] lg:-rotate-[15deg] -bottom-[12rem] md:-bottom-[8rem] lg:-bottom-[5rem] end-0 " />
     </section>
 )
 }
 
 export default LandingPage;
+
+// make a commit with this message "Feat: Implement MarqueeBar component in LandingPage component"
+// TODO: Adjust MarqueeBar on different screens
+// TODO: Bring Header, SpinningLogo, SpecialitiesBar, WatchVideoButton on top by z-index to set MarqueeBar at the bottom
