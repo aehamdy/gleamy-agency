ebad23d39fae1181160b50a88460f383284c0eee
aehamdy <ae.hamdy91@gmail.com>
2024-11-20 22:38:42 +0200

    Style: Recreate marquee text using css

--- src/components/MarqueeBar.jsx
+++ src/components/MarqueeBar.jsx
@@ -1,34 +1,51 @@
 /* eslint-disable react/prop-types */
 function MarqueeBar({ variant}) {
-    const content = [
-      'creative design',
-      'ui/ux',
-      'marketing',
-      'motion',
-      'animation',
-      'branding',
-    ];
-  
-    return (
-      <div
-        className={`absolute left-0 -translate-y-1/2 -translate-x-[50vw] w-[200vw] py-3 font-roboto text-center ${variant} z-0 overflow-hidden pointer-events-none`}
-      >
-        <div className="flex space-x-8 w-[120vw] animate-marquee ">
-          {Array(4)
-            .fill(content)
-            .flat()
-            .map((item, index) => (
-              <p
-                key={index}
-                className="relative before:absolute before:w-4 before:h-4 before:bg-accent before:rounded-full before:top-[50%] before:translate-y-[-50%] before:-start-6 px-8 whitespace-nowrap text-3xl uppercase"
-              >
-                {item}
-              </p>
-            ))}
+  const content = [
+    'creative design',
+    'ui/ux',
+    'marketing',
+    'motion',
+    'animation',
+    'branding',
+  ];
+
+  return (
+    // <div
+    //   className={`absolute left-0 -translate-y-1/2 -translate-x-[50vw] w-[200vw] py-3 font-roboto text-center ${variant} z-0 overflow-hidden pointer-events-none`}
+    // >
+    //   <div className="flex space-x-8 w-[120vw] animate-marquee ">
+    //     {Array(4)
+    //       .fill(content)
+    //       .flat()
+    //       .map((item, index) => (
+    //         <p
+    //           key={index}
+    //           className="relative before:absolute before:w-4 before:h-4 before:bg-accent before:rounded-full before:top-[50%] before:translate-y-[-50%] before:-start-6 px-8 whitespace-nowrap text-3xl uppercase"
+    //         >
+    //           {item}
+    //         </p>
+    //       ))}
+    //   </div>
+    // </div>
+    <div className="wrapper">
+      <div className="marquee-text">
+        <div className="marquee-text-track">
+        <p>creative design</p>
+        <p>ui/ux</p>
+        <p>marketing</p>
+        <p>motion</p>
+        <p>branding</p>
+        <p>animation</p>
+        <p aria-hidden="true">creative design</p>
+        <p aria-hidden="true">ui/ux</p>
+        <p aria-hidden="true">marketing</p>
+        <p aria-hidden="true">motion</p>
+        <p aria-hidden="true">branding</p>
+        <p aria-hidden="true">animation</p>
         </div>
       </div>
-    );
-  }
-  
-  export default MarqueeBar;
-  
+    </div>
+  );
+}
+
+export default MarqueeBar;
