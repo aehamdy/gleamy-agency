cac967147e4643661fe22aefff2204ad9e7f50f0
aehamdy <ae.hamdy91@gmail.com>
2024-11-21 00:45:00 +0200

    Style: Adjust MarqueeBar's position

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,8 +8,8 @@ function LandingPage() {
       <HeroHeading />
       {/* <MarqueeBar variant="text-black bg-white rotate-[50deg] md:rotate-[45deg] top-0 end-0 -translate-x-[9rem] md:-translate-x-[25rem]" /> */}
       {/* <MarqueeBar variant="text-white bg-[#212121] -rotate-[30deg] md:-rotate-[25deg] lg:-rotate-[15deg] -bottom-[12rem] md:-bottom-[8rem] lg:-bottom-[5rem] end-0 " /> */}
-      <MarqueeBar variant="text-white bg-[#212121] rotate-[-10deg] md:-rotate-[21deg] lg:-rotate-[15deg]" position="bottom-[5rem] md:bottom-[24rem] lg:bottom-[18rem] -start-[8rem] md:-start-[18rem]" />
-      <MarqueeBar variant="text-black bg-white rotate-[-10deg] md:-rotate-[21deg] lg:rotate-[-25deg]" position="bottom-[5rem] md:bottom-[24rem] lg:bottom-[18rem] -start-[8rem] md:end-[18rem]" />
+      <MarqueeBar variant="text-[#212121] bg-white rotate-[43deg] md:rotate-[42deg] lg:rotate-[47deg]" position="bottom-[-40rem] md:bottom-[-30rem] lg:bottom-[12rem] start-[-55rem] md:start-[-55rem] lg:start-[0] lg:end-[-42rem]" />
+      <MarqueeBar variant="text-white bg-[#212121] rotate-[-13deg] md:rotate-[-21deg] lg:rotate-[-15deg]" position="bottom-[11rem] md:bottom-[24rem] lg:bottom-[18rem] -start-[8rem] md:-start-[18rem]" />
     </section>
 )
 }
