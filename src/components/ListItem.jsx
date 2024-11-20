ecc74cf3c0917a37a5db5d60c6a39d0bdd3fad47
aehamdy <ae.hamdy91@gmail.com>
2024-11-20 15:52:08 +0200

    Feat: Implement NavLink component instead of Link to use 'isActive' capability

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function ListItem({ link, text, handleNavVisibility }) {
     return <li className="group w-full border-t last:border-b border-neutral-900 font-medium text-navItemClamp text-neutral-950 hover:text-accent">
-        <Link to={link} onClick={handleNavVisibility} className="relative inline-block w-full py-8 before:absolute before:w-full before:h-0 hover:before:h-full before:start-0 before:bottom-0 before:bg-neutral-950 before:transition-all before:duration-short before:z-[-1] duration-short">
+                    <NavLink
+                to={link}
+                onClick={handleNavVisibility}
+                className={({ isActive }) =>
+                    `relative inline-block w-full py-8 before:absolute before:w-full before:h-0 hover:before:h-full 
+                     before:start-0 before:bottom-0 before:bg-neutral-950 before:transition-all before:duration-short 
+                     before:z-[-1] duration-short ${isActive ? 'text-gray-300 bg-neutral-950' : ''}`
+                }
+            >
             {text.toUpperCase()}
-            </Link>
+            </NavLink>
         </li>
 }
 
