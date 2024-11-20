0953d089bccb708ff214253b6b633ea5695b1bd4
aehamdy <ae.hamdy91@gmail.com>
2024-11-20 16:48:51 +0200

    Feat: Implement OutlineText component in ErrorPage for more visually appealing UI

--- src/components/OutlineText.jsx
+++ src/components/OutlineText.jsx
@@ -2,7 +2,7 @@
 
 function OutlineText({ text }) {
     return(
-        <h2 className="absolute text-[300px] font-bold start-1/2 top-[50%] -translate-y-1/2 -translate-x-1/2 z-0 select-none"
+        <h2 className={`absolute ${text === "ERROR" ? 'text-[5.5rem] md:text-[6.5rem] lg:text-[8rem]' : 'text-[300px]'} font-bold start-1/2 top-[50%] -translate-y-1/2 -translate-x-1/2 z-0 select-none`}
         style={{WebkitTextStroke: "3px #404040", WebkitTextFillColor: 'transparent'}}>{text}</h2>
     )
 }
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import OutlineText from "../components/OutlineText";
 
 function ErrorPage() {
     return(
-        <div className="h-full content-center text-projectTextClamp text-neutral-300">
-            <div className="space-y-4">
+        <div className="relative h-full content-center text-projectTextClamp text-neutral-300">
+            <div className="absolute start-1/2 -translate-x-1/2 z-0">
+                <OutlineText text="ERROR" />
+            </div>
+            <div className="relative space-y-4">
             <h3>Oops!</h3>
             <h4>Something went wrong...</h4>
             <p>Click <Link to="/" className="text-accent underline">Home</Link> to go back</p>
