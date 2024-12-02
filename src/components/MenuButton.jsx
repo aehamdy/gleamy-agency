7615c8c0e644828606c8f437fa260fd5ca4679ca
aehamdy <ae.hamdy91@gmail.com>
2024-12-02 21:20:03 +0200

    Adjust NavList's ul and MenuButton position

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -8,7 +8,7 @@ function MenuButton({ text, handleNavVisibility }) {
 
     return(
         <button type="button" onClick={handleButtonClick} className="group flex flex-wrap items-center gap-3">
-            <div className="hidden md:block w-[50px] uppercase">{text}</div>
+            <div className={`${text.toLowerCase() !== "close" ? 'hidden' : 'block'} md:block w-[50px] uppercase`}>{text}</div>
             <div className="flex flex-col gap-1.5 w-7">
                 <span className={`w-full h-1 bg-white ${text === "Close" && 'rotate-45'} duration-short`}></span>
                 <span className={`${text === "Close" ? 'w-full -rotate-45 -translate-y-2.5' : 'w-1/2'} h-1 bg-white duration-short`}></span>
--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -24,10 +24,10 @@ function NavList({ isNavExpanded, handleNavVisibility }) {
 
     return (
         <nav className={`grid place-items-center absolute w-full h-[100dvh] ${isNavExpanded ? 'top-0 visible' : 'top-[-100%] invisible'} start-0 bg-accent z-50 duration-500`}>
-            <div className="justify-self-end -mb-12 me-[4%]">
+            <div className="absolute justify-self-end top-[10%] -mb-12 me-[4%]">
             <MenuButton text="Close" isNavExpanded={isNavExpanded} handleNavVisibility={handleNavVisibility} />
             </div>
-            <ul className="w-full">
+            <ul className="w-full absolute top-1/2 translate-y-[-40%]">
                 {listItems.map((item, i) => (
                     <ListItem key={i} link={item.link} text={item.text} handleNavVisibility={handleNavVisibility} />
                 ))}
